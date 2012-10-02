b27b506f3d5ccdc5a5f6650add45198991e74ebe
Lee Delarm <protoshell@gmail.com>
2012-10-02 11:07:41 +0200

    Cutting open cat food can
    Started development of resume display, used scaffolding for user to see
    how syntax worked. Start of resume object development in progress,
    starting with controller.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PirateCatData::Application.config.secret_token = 'c941c5eb31f6d61a334067fd0d9ca6c0754f0cfca7478eb3610fb077701ed3f95f1352454e42b47157c53f14cd85916c947f7cfb87032f86f8b8dcfbc0eb797e'
